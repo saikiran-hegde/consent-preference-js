7f13aded4e80a40432865f5c852e062b999f0c7b
saikiran-hegde <saikiran.hegde@zeotap.com>
2021-06-18 00:12:12 +0530

    Collapsible list & toggle switch added

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ function setValue() {
   icomId.innerText = data.icomId;
   hhId.innerText = data.hhId;
   ucId.innerText = data.ucId;
+
+  // Set toggle switch
+  document.getElementById('global-toggle-switch').checked = true;
+  document.getElementById('obtm-toggle-switch').checked = true;
 }
 
 setValue();
+
+function checkboxValueChange(event) {
+  console.log(`${event.currentTarget.id} - ${event.currentTarget.checked}`);
+}
+
+function showHide() {
+  const collapseDiv = document.getElementById('collapse-icon-div'),
+    consentDiv = document.getElementById('consent-preference');
+  if (consentDiv.style.display === 'none' || consentDiv.style.display === '') {
+    collapseDiv.style.transform = "rotate(90deg) translate(12px, 14px)";
+    consentDiv.style.display = 'grid';
+  } else {
+    collapseDiv.style.transform = "rotate(0deg)";
+    consentDiv.style.display = 'none';
+  }
+}
