477a39c8d763f9abfbcc5801474ec2eed5fa26af
saikiran-hegde <saikiran.hegde@zeotap.com>
2021-06-22 23:58:37 +0530

    Added fetch api to read profile data and change directory structure to bundle with webpack

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ const icomId = document.getElementById('icomId'),
   hhId = document.getElementById('hhId'),
   ucId = document.getElementById('ucId');
 
-const data = {
-  icomId: 1231241,
-  hhId: 123456,
-  ucId: 112234
-};
+const urlData = {
+  'search': {
+      'icoms_account_uid': ['1111111138']
+  }
+}
 
-function setValue() {
-  icomId.innerText = data.icomId;
-  hhId.innerText = data.hhId;
-  ucId.innerText = data.ucId;
+function setValue(icomIdVal, hhIdVal, ucIdVal) {
+  icomId.innerText = icomIdVal;
+  hhId.innerText = hhIdVal;
+  ucId.innerText = ucIdVal;
 
   // Set toggle switch
   document.getElementById('global-toggle-switch').checked = true;
@@ -37,8 +37,26 @@ function showHide() {
 const profileContainer = document.getElementsByClassName('profile-container')[0];
 profileContainer.style.display = 'none';
 
+function fetchProfileData(accessToken) {
+  fetch('http://idgraph-qa-eu.zeotap.net/graph-manager/v1/orgs/1746/region/EU/users/_search',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify(urlData)
+      })
+      .then(response => response.json())
+      .then(response => {
+        data = response[0];
+        setValue(data.ids.icoms_account_uid[0].icoms_account_uid, data.ucid, data.ids.household_id[0].household_id);
+        profileContainer.style.display = 'block';
+      });
+}
+
 const authClient = new OktaAuth({
-  url: 'https://login-staging.zeotap.com/',
   issuer: 'https://login-staging.zeotap.com/oauth2/default',
   clientId: '0oa1kkxu0r6xSg2j40x7',
   redirectUri: 'http://localhost:8080'
@@ -47,36 +65,19 @@ const authClient = new OktaAuth({
 if (authClient.isLoginRedirect()) {
   authClient.token.parseFromUrl()
     .then(data => {
-      const { idToken } = data.tokens;
-      authClient.tokenManager.add('idToken', idToken);
-      console.log(idToken);
-      setValue();
-      profileContainer.style.display = 'block';
+      authClient.tokenManager.add('accessToken', data.tokens.accessToken);
+      fetchProfileData(data.tokens.accessToken.accessToken);
     });
 } else {
-  authClient.tokenManager.get('idToken')
-    .then(idToken => {
-      if (idToken) {
-        console.log(idToken);
-        setValue();
-        profileContainer.style.display = 'block';
+  authClient.tokenManager.get('accessToken')
+    .then(accessToken => {
+      if (accessToken) {
+        fetchProfileData(accessToken.accessToken)
       } else {
-        // With redirecting user to Okta Page
-        // authClient.token.getWithRedirect({
-        //   responseType: 'id_token'
-        // });
-
-        // With user credentials
-        const username = prompt('Username');
-        const password = prompt('Password');
-
-        authClient.signInWithCredentials({username, password})
-        .then(transaction => {
-          authClient.token.getWithRedirect({
-            sessionToken: transaction.sessionToken,
-            responseType: 'id_token'
-          });
-        })
+        authClient.token.getWithRedirect({
+          responseType: 'token',
+          loginHint: 'saikiran.hegde@zeotap.com'
+        });
       }
     })
 }
